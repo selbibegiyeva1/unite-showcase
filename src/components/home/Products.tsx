07858edfafcc80b9f2c53975ef7435ac64f5d2d4
Selbi <selbibegiyeva1@gmail.com>
2025-12-24 15:58:00 +0500

    Switch buttons img

--- src/components/home/Products.tsx
+++ src/components/home/Products.tsx
@@ -22,12 +22,12 @@ export default function Products() {
                     className={`switch ${activeCategory === "games" ? "switch--active" : "switch--inactive"}`}
                     onClick={() => setActiveCategory("games")}
                 >
-                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M9.16667 6.41634C9.16667 7.93512 7.93545 9.16634 6.41667 9.16634C4.89788 9.16634 3.66667 7.93512 3.66667 6.41634C3.66667 4.89756 4.89788 3.66634 6.41667 3.66634C7.93545 3.66634 9.16667 4.89756 9.16667 6.41634Z" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M12.8333 12.833H18.3333V18.333H12.8333V12.833Z" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M5.04167 14.208L2.75 15.583L5.04167 16.958L6.41667 19.2497L7.79167 16.958L10.0833 15.583L7.79167 14.208L6.41667 11.9163L5.04167 14.208Z" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
-                        <path d="M15.5833 3.20801L19.25 9.16634H11.9167L15.5833 3.20801Z" stroke="white" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <img
+                        src="/home/toy.png"
+                        alt="toy"
+                        style={{ width: 22 }}
+                        className={activeCategory !== "games" ? "switch--inactive-img" : ""}
+                    />
                     <span>Игры</span>
                 </button>
                 <button
@@ -35,9 +35,12 @@ export default function Products() {
                     className={`switch ${activeCategory === "business" ? "switch--active" : "switch--inactive"}`}
                     onClick={() => setActiveCategory("business")}
                 >
-                    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.08333 18.583H10.75M13.4167 18.583H10.75M10.75 18.583V15.958M10.75 15.958H16.75C17.8546 15.958 18.75 15.0626 18.75 13.958V6.58301C18.75 5.47844 17.8546 4.58301 16.75 4.58301H4.75C3.64543 4.58301 2.75 5.47844 2.75 6.58301V13.958C2.75 15.0626 3.64543 15.958 4.75 15.958H10.75Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <img
+                        src="/home/desktop.png"
+                        alt="desktop"
+                        style={{ width: 22 }}
+                        className={activeCategory !== "business" ? "switch--inactive-img" : ""}
+                    />
                     <span>Программы</span>
                 </button>
             </div>
