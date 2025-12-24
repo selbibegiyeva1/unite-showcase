5f668caa031a0dcf2f94db3675c4fa662ad6c9a0
Selbi <selbibegiyeva1@gmail.com>
2025-12-24 13:01:06 +0500

    Faq and Footer component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,22 +2,32 @@ import Slider from "../components/home/Slider"
 import SliderPopular from "../components/home/SliderPopular"
 import Products from "../components/home/Products"
 import News from "../components/home/News"
+import Faq from "../components/home/Faq"
+
+import Footer from "../components/layout/Footer"
 
 function Home() {
     return (
         <div>
-            <div className="mb-15">
-                <Slider />
-            </div>
-            <SliderPopular />
+            <div className="max-w-255 m-auto px-4 text-white">
+                <div className="mb-15">
+                    <Slider />
+                </div>
+                <SliderPopular />
 
-            <div className="pt-10 pb-15">
-                <Products />
-            </div>
+                <div className="pt-10 pb-15">
+                    <Products />
+                </div>
 
-            <div className="pb-15">
-                <News />
+                <div className="pb-15">
+                    <News />
+                </div>
+
+                <div className="pb-46">
+                    <Faq />
+                </div>
             </div>
+            <Footer />
         </div>
     )
 }
