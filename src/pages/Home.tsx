32d55b7b6f07c23725a31f0b48e1d1eee950543d
Selbi <selbibegiyeva1@gmail.com>
2025-12-24 12:08:43 +0500

    News component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Slider from "../components/home/Slider"
 import SliderPopular from "../components/home/SliderPopular"
 import Products from "../components/home/Products"
+import News from "../components/home/News"
 
 function Home() {
     return (
@@ -12,6 +13,10 @@ function Home() {
 
             <div className="pt-10 pb-15">
                 <Products />
+            </div>
+
+            <div className="pb-15">
+                <News />
             </div>
         </div>
     )
