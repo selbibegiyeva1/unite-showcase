e77c593e2c8eba92d17c562f95419981bf01e9eb
Selbi <selbibegiyeva1@gmail.com>
2026-01-16 10:25:38 +0500

    Metrika env commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: '0.0.0.0', // Allow access from outside Docker container
-    port: 5173,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      {
+        name: 'html-transform',
+        transformIndexHtml(html) {
+          const yandexMetrikaId = env.VITE_YANDEX_METRIKA_ID;
+          return html.replace(/YANDEX_METRIKA_ID_PLACEHOLDER/g, yandexMetrikaId);
+        },
+      },
+    ],
+    server: {
+      host: '0.0.0.0', // Allow access from outside Docker container
+      port: 5173,
+    },
+  };
 })
